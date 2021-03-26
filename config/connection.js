9a92cb2d7d267c058ec72d722c7fa8bb13373436
Jito Chadha <jitochadha@gmail.com>
2021-03-26 12:06:40 -0700

    updated env files

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const sequelize = process.env.JAWSDB_URL
     ? new Sequelize(process.env.JAWSDB_URL)
     : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-        host: 'vkh7buea61avxg07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        host: 'localhost',
         dialect: 'mysql',
         port: 3306
     });
